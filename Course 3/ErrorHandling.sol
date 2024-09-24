4846a72ceb100a30342309c04faca554cd36c3a9
ACMolina0805 <arjencatherinemolina@gmail.com>
2024-09-24 19:56:33 +0800

    Update ErrorHandling.sol

--- Course 3/ErrorHandling.sol
+++ Course 3/ErrorHandling.sol
@@ -2,20 +2,24 @@
 pragma solidity ^0.8.13;
 
 contract Volleyball {
-    
-    function gameStatus(uint teamA, uint teamB) public pure {
 
-        //Checks if one team has already reached 25
-        if(teamA < 25 && teamB < 25) {
-            //Checks if both teams ties at 24
-            if (teamA == 24 && teamB == 24){
-                revert("Deuce: Game cannot finish yet.");
-            }
-            revert("No one has reached 25. Game is not yet done.");
+    uint public teamA = 0;
+    uint public teamB = 0;
+
+    function pointSystem(uint point1, uint point2) public {
+        // Checks if one team has already reached 25
+        if (teamA >= 25 || teamB >= 25) {
+            revert("Cannot add more points! Game already finished.");
         }
+        
+        teamA += point1;
+        teamB += point2;
 
+    }
+    
+    function gameStatus() public view {
 
-        // Checks if one of the teams has won by more than 1 point
+        // Checks if one of the teams has won by more than 1 point when a deuce occurs.
         require((teamA >= 24 && teamA == teamB + 2) || (teamB >= 24 && teamB == teamA + 2),
                 "No one has won the game yet. A team must be ahead by at least 2 points after a deuce.");
     }
